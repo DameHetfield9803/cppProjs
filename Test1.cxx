06597f5709e83fd4d42127b0fd90abd0660af2c2
DameHetfield9803 <135241537+DameHetfield9803@users.noreply.github.com>
2023-08-16 21:20:11 +0800

    Create Test1.cxx
    Bla bla bla

---
+++ Test1.cxx
@@ -0,0 +1,14 @@
+#include <iostream>
+
+double prices(){
+  double gpu, cpu;
+  gpu = 699.9999;
+  cpu = 449.999;
+  double total = cpu + gpu;
+  return 0;
+}
+
+int main(){
+  std::cout << "Ram: 32gb Storage: 2TB "; 
+  return 0;
+}
