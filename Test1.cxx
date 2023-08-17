8b7c7517c8c20c03a16f05bbb4a99899790ba2ea
DameHetfield9803 <135241537+DameHetfield9803@users.noreply.github.com>
2023-08-18 00:08:04 +0800

    Update Test1.cxx

--- Test1.cxx
+++ Test1.cxx
@@ -8,6 +8,21 @@ double prices(){
   return 0;
 }
 
+double salarySG(){
+  double salary;
+  std::cout << "Please enter your salary ($) > ";
+  salary = std::cin >> ; 
+  double cpf = 0.20 * salary; 
+  double cpfTotal = (salary * 0.17) + cpf; 
+  double remainingSalary = salary * 0.83;
+  std::cout << "Your remaining salary is " + remainingSalary + " and your CPF in total is " + cpfTotal + " \n";
+  bool race;
+  std::cout << "Is your race Chinese?";
+  if (race == "yes" || race == "y" || race == "Yes" || race= "YES"){
+    std::cout << "...";
+ }  
+}
+
 int main(){
   std::cout << "Ram: 32gb Storage: 2TB "; 
   return 0;
